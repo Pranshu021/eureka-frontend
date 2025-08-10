78277acfe81b08ad6c6eb1a129d177c6e4e676b9
Pranshu021 <pranshu.duneja@wipro.com>
2025-08-10 19:33:52 +0530

    Fixed build error in PageNotFound.tsx

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,10 +1,7 @@
 // src/pages/NotFound.tsx
-import { useNavigate } from "react-router-dom";
 import { Box, Typography } from "@mui/material";
 
 export default function PageNotFound() {
-  const navigate = useNavigate();
-
   return (
     <Box>
         <Typography variant="h2">Oops! 404 PAGE NOT FOUND!</Typography>
